596d0dfb249fbf24671c3b9bf2eabc6fecb511f7
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-05-10 23:37:23 +0900

    Ternary Operator with user input

--- test.py
+++ test.py
@@ -1,16 +1,3 @@
-
-
-is_member = input("You are a member (yes/no): ").lower();
-total_amount = int(input("total amount: "));
-total_discount_amount = 0;
-
-if is_member == "yes":
-    if total_amount >= 15000 and total_amount <= 20000:
-        total_discount_amount = 500
-    elif total_amount >= 20000 and total_amount <= 50000:
-        total_discount_amount = total_amount * 3/100
-    elif total_amount > 50000:
-        total_discount_amount = total_amount * 6/100
-
-subtotal_amount = total_amount - total_discount_amount;
-print("Total Amount: ", subtotal_amount);
+billing_number = int(input("billing number: "))
+result = "bill already paid!" if billing_number == 12345 else "transaction successful!"
+print(result)
--- variable_datatypes_controlflow/control_flow.py
+++ variable_datatypes_controlflow/control_flow.py
@@ -189,6 +189,12 @@ result = "Adult" if age >= 18 else "Not Adult"
 print(result)
 
 
+#========================
+print("Ternary Operator with user input")
+#==========================
+billing_number = int(input("billing number: "))
+result = "bill already paid!" if billing_number == 12345 else "transaction successful!"
+print(result)
 
 
 
