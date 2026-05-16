45902d49f1e591fe76c7f319066986a81a6d422a
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-05-16 23:09:05 +0900

    code remove

--- problem_solving/class_1_to_9/7_password_strength_checker/3_ternary_operator.py
+++
@@ -1,3 +0,0 @@
-password = input("Enter password: ")
-result = "Strong Password" if len(password) >= 8 and any(char.isdigit() for char in password) else "Weak Password"
-print(result)
--- problem_solving/class_1_to_9/7_password_strength_checker/4_logical_operator.py
+++
@@ -1,5 +0,0 @@
-password = input("Enter password: ")
-if len(password) >= 8 and any(char.isdigit() for char in password):
-    print("Strong Password")
-elif len(password) < 8 or not any(char.isdigit() for char in password):
-    print("Weak Password")
