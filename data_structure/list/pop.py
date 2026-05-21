a923740d575a08d3344cafca213f04b577eb5a92
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-05-21 23:14:14 +0900

    pop method

---
+++ data_structure/list/pop.py
@@ -0,0 +1,4 @@
+fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+removed_fruit = fruits.pop(1);
+print(fruits); #Output: ['apple', 'cherry', 'date', 'elderberry']
+print("Removed fruit:", removed_fruit); #Output: Removed fruit: date
--- data_structure/list/tempCodeRunnerFile.py
+++ data_structure/list/tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-fruits
+banana
