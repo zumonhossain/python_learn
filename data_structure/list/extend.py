a9275e9a5a5ab501613a08c697f15395fc2989f6
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-05-21 22:23:49 +0900

    extend

--- data_structure/list/extend.py
+++ data_structure/list/extend.py
@@ -1,6 +1,19 @@
+# list1 = ["apple", "banana"]
+# list2 = ["cherry", "date"]
+
+# list1.extend(list2)
+
+# print(list1)
+
 list1 = ["apple", "banana"]
 list2 = ["cherry", "date"]
+list3 = ["elderberry", "fig"]
+list4 = ["grape", "honeydew"]
+list5 = ["kiwi", "lemon"]
+
 
 list1.extend(list2)
-
-print(list1)
+list1.extend(list3)
+list1.extend(list4)
+list1.extend(list5)
+print(list1) 
