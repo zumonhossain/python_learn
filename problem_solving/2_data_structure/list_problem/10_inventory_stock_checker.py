be40f6d3e60b1ec72896db09d5e48979c5547571
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-05-22 20:19:01 +0900

    Inventory Stock Checker problem solved

---
+++ problem_solving/2_data_structure/list_problem/10_inventory_stock_checker.py
@@ -0,0 +1,5 @@
+product_qty = [0, 5, 12, 0, 3];
+product_qty[0] = 8;
+print("Total products:", len(product_qty));
+print("Max stock:", max(product_qty), "|", "Min stock:", min(product_qty));
+print("Updated stock:", product_qty);
