b5290b12b139b4d39e99e0fd2a8010a8c0f46f59
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-05-22 17:55:40 +0900

    shopping cart manager problem solve

---
+++ problem_solving/2_data_structure/list_problem/1_shopping_cart_manager.py
@@ -0,0 +1,13 @@
+output_cart = [];
+
+input_item1 = input("Enter item 1:");
+output_cart.append(input_item1);
+
+input_item2 = input("Enter item 2:");
+output_cart.append(input_item2);
+
+input_item3 = input("Enter item 3:");
+output_cart.append(input_item3);
+
+print("Your cart:", output_cart);
+print("Total items:", len(output_cart));
