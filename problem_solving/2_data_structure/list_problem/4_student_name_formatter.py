1dcd25d969b348aa02ebc520f1b59dc0d1a06d4d
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-05-22 18:48:32 +0900

    Student Name Formatter problem solved

---
+++ problem_solving/2_data_structure/list_problem/4_student_name_formatter.py
@@ -0,0 +1,13 @@
+students = [];
+
+input1 = input("Enter name 1:");
+students.append(input1);
+
+input2 = input("Enter name 2:");
+students.append(input2);
+
+input3 = input("Enter name 3:");
+students.append(input3);
+
+print("Students", students);
+print("Total students:", len(students));
