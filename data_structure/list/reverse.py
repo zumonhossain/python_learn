7407f7fc8cd5bc8a8c49db0095ac63551215a9d4
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-05-22 17:10:10 +0900

    reverse method

---
+++ data_structure/list/reverse.py
@@ -0,0 +1,12 @@
+fruits = ["elderberry", "apple", "banana", "cherry", "date", "cherry", "cherry"];
+result = reversed(fruits);
+print(list(result)); # ['cherry', 'cherry', 'date', 'cherry', 'banana', 'apple', 'elderberry']
+
+
+numbers = [5, 2, 9, 1, 5, 6];
+result = reversed(numbers);
+print(list(result)); # [6, 5, 1, 9, 2, 5]
+
+numbers = [1, 2, 3, 4, 5];
+result = reversed(numbers);
+print(list(result)); # [5, 4, 3, 2, 1]
