fce26aefcaa7f1b31914e4177d3ef4942d29c3fe
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-05-22 16:45:23 +0900

    len method

---
+++ data_structure/list/len.py
@@ -0,0 +1,17 @@
+
+#===========================================
+print("----------- Using len() function to find the length of a list: ------------")  # 10;
+#===========================================
+# Description: This code demonstrates how to use the len() function to find the length of a
+fruits = ["apple", "banana", "cherry", "date", "cherry", "cherry", "elderberry"];
+result = len(fruits);
+print(result); # Output: 7, because the list contains 7 elements.
+
+#============================================
+print("---------- Length of numbers list: ------------")  # 10
+#============================================
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+empty = []
+
+print("total elements in numbers list:", len(numbers))  # 10
+print("total elements in empty list:", len(empty))    # 0
