ae0042a532f71649236bd54296db7da6496c9c86
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-05-27 00:14:40 +0900

    Nested List

---
+++ data_structure/list/nested_list.py
@@ -0,0 +1,11 @@
+fruits = [
+    ["Apple", "Banana", "Cherry", "Strawberry"],
+    ["Mango", "Orange", "Peach", "Watermelon"],
+    ["Grapes", "Pineapple", "Kiwi", "Blueberry"]
+]
+print(fruits)  # Output: [['Apple', 'Banana', 'Cherry', 'Strawberry'], ['Mango', 'Orange', 'Peach', 'Watermelon'], ['Grapes', 'Pineapple', 'Kiwi', 'Blueberry']]
+print(fruits[0])  # Output: ['Apple', 'Banana', 'Cherry', 'Strawberry']
+print(fruits[0][1])  # Output: Banana
+print(fruits[1][3])  # Output: Watermelon
+print(fruits[2][0])  # Output: Grapes
+print(fruits[2][3])  # Output: Blueberry
