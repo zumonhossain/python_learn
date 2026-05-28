581d779347cfdca8fc397f40914de40e1cef5277
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-05-28 22:45:00 +0900

    for loop and while loop

--- data_structure/list/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-fruits = ["apple", "banana", "cherry", "date", "elderberry"];
-# del fruits[1]; #single element deletion
-# print(fruits); #Output: ['apple', 'cherry', 'date', 'elderberry']
-
-# fruits = ["apple", "banana", "cherry", "date", "elderberry"];
-# del fruits[1:3]; #deleting a slice of elements
-# print(fruits); #Output: ['apple', 'elderberry']
---
+++ data_structure/list/while_loop.py
@@ -0,0 +1,21 @@
+# count = 5;
+# while count > 0:
+#     print(count); #Output: 5
+#                    #        4
+#                    #        3
+#                    #        2
+#                    #        1
+#     count -= 1;
+
+
+
+
+
+count = 1;
+while count <= 5:
+    print(count); #Output: 1
+                   #        2
+                   #        3
+                   #        4
+                    #        5
+    count += 1;
