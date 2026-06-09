8cf39387c34bb34ab4efd6472b6eda11f218161b
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-06-09 09:30:33 +0900

    Student Grade System Problem Statement Solved

--- problem_solving/1_class_1_to_9/1_voting_eligibility_checker/voting_eligibility_checker.py
+++ problem_solving/1_class_1_to_9/1_voting_eligibility_checker/voting_eligibility_checker.py
@@ -1,5 +1,6 @@
-age = int(input("Enter your age: "));
+age = int(input("Enter your age:"));
+
 if age >= 18:
-    print("Eligible for voting");
+	print("Eligible for voting");
 else:
-    print("Not eligible for voting");
+	print("Not eligible for voting");
