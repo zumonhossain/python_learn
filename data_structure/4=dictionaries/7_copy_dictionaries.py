1a49cb223e9edba3e4abd30e23e38773e4786cc3
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-06-27 18:40:07 +0900

    Copy Dictionaries

---
+++ data_structure/4=dictionaries/7_copy_dictionaries.py
@@ -0,0 +1,28 @@
+   
+
+
+
+# Copy a Dictionary
+print("--- Make a copy of a dictionary with the copy() method: ---");
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+
+
+
+
+# dict()
+print("--- Make a copy of a dictionary with the dict() function: ---");
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
