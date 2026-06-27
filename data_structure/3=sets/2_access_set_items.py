568c5e1293e80cca35eeebd81622f68b4fc6636d
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-06-27 17:42:31 +0900

    Access Set Items

---
+++ data_structure/3=sets/2_access_set_items.py
@@ -0,0 +1,28 @@
+
+
+
+# Access Items
+print("------------------- Loop through the set, and print the values: ---------------------");
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+
+
+# Check if "banana" is present in the set:
+print("------------------- Check if 'banana' is present in the set: ---------------------");
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+
+
+
+# Check if "banana" is NOT present in the set:
+print("------------------- Check if 'banana' is NOT present in the set: ---------------------");
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" not in thisset)
