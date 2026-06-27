81c9e60557744f9c7f9197c8e48353997e92540d
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-06-27 17:26:23 +0900

    Join Tuples

---
+++ data_structure/2=tuples/6_join_tuples.py
@@ -0,0 +1,19 @@
+
+# Join Two Tuples
+print("------------------- Join two tuples: ---------------------");
+
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+
+
+# Multiply Tuples
+print("------------------- Multiply the fruits tuple by 2: ---------------------");
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
