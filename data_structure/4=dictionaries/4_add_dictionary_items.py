2be5f1d3f7b3fd67d3f39314d30a4e4120e32930
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-06-27 18:30:47 +0900

    Add Dictionary Items

---
+++ data_structure/4=dictionaries/4_add_dictionary_items.py
@@ -0,0 +1,25 @@
+
+
+
+# Adding Items
+print("--- Adding an item to the dictionary is done by using a new index key and assigning a value to it: ---");
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+
+
+
+
+# Update Dictionary
+print("--- Add a color item to the dictionary by using the update() method: ---");
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
