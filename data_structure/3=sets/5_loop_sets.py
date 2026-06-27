c599347809ff2009fa9bfddd3d6cf7ba875d7cfe
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2026-06-27 17:51:34 +0900

    Loop Sets

---
+++ data_structure/3=sets/5_loop_sets.py
@@ -0,0 +1,10 @@
+
+
+
+# Loop Items
+print("------------------- Loop through the set, and print the values: ---------------------");
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
